4d34c8bbb945c131a745f4f92d5c817ddd29a019
kadirgokalp <kadirgokalp1@gmail.com>
2022-06-01 20:24:47 +0300

    Sidebar is updated with dropdown , TestPage is done and added to the Regions Menu.

--- my-project/src/components/Regions/Test/Test.js
+++ my-project/src/components/Regions/Test/Test.js
@@ -36,14 +36,6 @@ const Test = () => {
   return (
     <>
     <Navbar />
-    {/* <div className="card">
-      <div className="card-header">React Fetch GET - BezKoder.com</div>
-      <div className="card-body">
-        <div className="input-group input-group-sm">
-          <button className="btn btn-sm btn-primary" onClick={getAllData}>Start System</button>
-        </div>
-      </div>
-    </div> */}
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
